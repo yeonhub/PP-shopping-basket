06ff5e17253bae107ead7848de8f29022c16c59e
yeonhub-desktop <lsy3237@gmail.com>
2023-12-12 00:19:40 +0900

    [FIX] addCart 중복 오류 수정

--- main.js
+++ main.js
@@ -52,11 +52,12 @@ let list = [
 // init
 show();
 
+// 상품 가격 변환
 function priceToString(price) {
     return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 };
 
-// re
+// 재할당
 function re() {
     $ul_box = get('#container .inner .ul_box');
     $ul = get('#container .inner ul');
@@ -68,7 +69,7 @@ function re() {
     $chk = getAll('#wrapB #container .cartUl_box ul li .xi-check-circle-o')
 };
 
-// list 출력
+// 상품 목록에 list 출력
 function show() {
     $ul.innerHTML = ''
     for (let i = 0; i < list.length; i++) {
@@ -87,11 +88,12 @@ function show() {
     };
     re();
     pic();
-    jjim();
+    // jjim();
+    jjimEventListeners();
     add();
 }
 
-// hover (썸네일 호버)
+// thumbnail hover 시 이미지 변환
 function pic() {
     $li.forEach((element, idx) => {
         element.addEventListener('mouseenter', e => {
@@ -104,231 +106,219 @@ function pic() {
     })
 }
 
-// JJIM
-function jjim() {
-    $ht.forEach(elemet => {
-        elemet.addEventListener('click', e => {
-            if (e.currentTarget.classList.contains('xi-heart-o')) {
-                e.currentTarget.classList.remove('xi-heart-o');
-                e.currentTarget.classList.add('xi-heart');
-                let id = e.target.parentElement.dataset.id;
-                idList.push(id)
-            } else {
-                e.currentTarget.classList.remove('xi-heart');
-                e.currentTarget.classList.add('xi-heart-o');
-                let id = e.target.parentElement.dataset.id;
-                idList = idList.filter((ele) => ele !== id)
-            }
-        })
+// 찜
+// function jjim() {
+//     $ht.forEach(elemet => {
+//         elemet.addEventListener('click', e => {
+//             if (e.currentTarget.classList.contains('xi-heart-o')) {
+//                 e.currentTarget.classList.remove('xi-heart-o');
+//                 e.currentTarget.classList.add('xi-heart');
+//                 let id = e.target.parentElement.dataset.id;
+//                 idList.push(id)
+//             } else {
+//                 e.currentTarget.classList.remove('xi-heart');
+//                 e.currentTarget.classList.add('xi-heart-o');
+//                 let id = e.target.parentElement.dataset.id;
+//                 idList = idList.filter((ele) => ele !== id)
+//             }
+//         })
+//     });
+// }
+function updateJjimList(id, isAdd) {
+    if (isAdd) {
+        idList.push(id);
+    } else {
+        idList = idList.filter((ele) => ele !== id);
+    }
+}
+function updateHeartButton(element, isFilled) {
+    if (isFilled) {
+        element.classList.remove('xi-heart-o');
+        element.classList.add('xi-heart');
+    } else {
+        element.classList.remove('xi-heart');
+        element.classList.add('xi-heart-o');
+    }
+}
+function jjimEventListeners() {
+    $ht.forEach(element => {
+        element.addEventListener('click', e => {
+            let id = e.target.parentElement.dataset.id;
+            let isAdd = e.currentTarget.classList.contains('xi-heart-o');
+            updateJjimList(id, isAdd);
+            updateHeartButton(e.currentTarget, isAdd);
+        });
     });
+};
+
+
+// cart 상품 증가/감소
+// function cartPM(incBtn, redBtn) {
+//     incBtn.addEventListener('click', e => {
+//         let id = e.currentTarget.parentElement.dataset.id
+//         if (list[id].stock !== 0) {
+//             list[id].stock--
+//             list[id].cartStock++
+//             e.currentTarget.nextElementSibling.nextElementSibling.textContent = list[id].cartStock;
+//             show();
+//         }
+//     })
+
+//     redBtn.addEventListener('click', e => {
+//         let id = e.currentTarget.parentElement.dataset.id
+//         if (list[id].cartStock !== 1) {
+//             list[id].stock++
+//             list[id].cartStock--
+//             e.currentTarget.nextElementSibling.textContent = list[id].cartStock;
+//             show()
+//         }
+//     })
+// }
+
+function updateStock(id, stockDelta, cartStockDelta) {
+    list[id].stock += stockDelta;
+    list[id].cartStock += cartStockDelta;
 }
 
-// cart +/- (카트 안에 상품 증가/감소)
+// cart 상품 증가/감소
 function cartPM(incBtn, redBtn) {
     incBtn.addEventListener('click', e => {
         let id = e.currentTarget.parentElement.dataset.id
-        if (list[id].stock !== 0) {
-            list[id].stock--
-            list[id].cartStock++
-            e.currentTarget.nextElementSibling.nextElementSibling.textContent = list[id].cartStock;
-            show();
+        if(list[id].stock !== 0){
+            updateStock(id, -1, 1);
         }
+        e.currentTarget.nextElementSibling.nextElementSibling.textContent = list[id].cartStock;
+        show();
     })
-
     redBtn.addEventListener('click', e => {
         let id = e.currentTarget.parentElement.dataset.id
-        if (list[id].cartStock !== 1) {
-            list[id].stock++
-            list[id].cartStock--
-            e.currentTarget.nextElementSibling.textContent = list[id].cartStock;
-            show()
+        if(list[id].cartStock !== 1){
+            updateStock(id, 1, -1);
         }
+        e.currentTarget.nextElementSibling.textContent = list[id].cartStock;
+        show()
     })
 }
+
 
 // remove cart item (장바구니 상품 제거)
-function cartRemove(remBtn) {
-    remBtn.addEventListener('click', e => {
-        let id = e.currentTarget.parentElement.dataset.id
-        list[id].stock += list[id].cartStock;
-        list[id].cartStock = 0;
-        list[id].cart = false
-        list[id].heart = false
-        e.currentTarget.parentElement.remove()
-        cartList = cartList.filter(item => item !== id);
-        show()
-    })
+// function cartRemove(remBtn) {
+//     remBtn.addEventListener('click', e => {
+//         let id = e.currentTarget.parentElement.dataset.id
+//         list[id].stock += list[id].cartStock;
+//         list[id].cartStock = 0;
+//         list[id].cart = false
+//         list[id].heart = false
+//         e.currentTarget.parentElement.remove()
+//         cartList = cartList.filter(item => item !== id);
+//         show()
+//     })
+// }
+function removeFromCart(id) {
+    updateStock(id, list[id].cartStock, -list[id].cartStock);
+    list[id].cart = false;
+    list[id].heart = false;
+    cartList = cartList.filter(item => item !== id);
 }
 
-// 찜 상품 담기
-$jjimCart.addEventListener('click', e => {
-    for (let i = 0; i < idList.length; i++) {
-        list[idList[i]].heart = true;
-    }
-    deduList = idList.filter(item => !cartList.includes(item));
-    if (deduList.length != 0) {
-        for (let i = 0; i < deduList.length; i++) {
-
-            list[deduList[i]].cart = true;
-            list[deduList[i]].stock--;
-            list[deduList[i]].cartStock++;
-            let li = document.createElement('li');
-            let incBtn = document.createElement('button');
-            let redBtn = document.createElement('button');
-            let span = document.createElement('span');
-            let remBtn = document.createElement('button');
-            li.dataset.id = list[deduList[i]].id
-            li.innerHTML = `
-            <img src="images/img${list[deduList[i]].id}-0.jpg" alt="">
-            `
-            incBtn.classList.add('incre')
-            redBtn.classList.add('redu')
-            span.innerHTML = list[deduList[i]].cartStock
-            remBtn.classList.add('remove')
-
-            incBtn.innerHTML = `<i class="xi-angle-up"></i>`
-            redBtn.innerHTML = `<i class="xi-angle-down"></i>`
-            remBtn.innerHTML = `<i class="xi-cart-remove"></i>`
-
-            $cartUl.append(li)
-            li.append(incBtn)
-            li.append(redBtn)
-            li.append(span)
-            li.append(remBtn)
-
-            cartPM(incBtn, redBtn)
-            cartRemove(remBtn)
-
-            list[deduList[i]].heart = false;
-        }
-        idList = []
-        cartList = []
-        deduList = []
-    } else {
-        idList = []
-        cartList = []
-        deduList = []
-    }
-    // heart remove
-    let heartElement = document.querySelector('.xi-heart');
-    if (heartElement) {
-        heartElement.classList.remove('xi-heart');
-        heartElement.classList.add('xi-heart-o');
-    }
-    // heart state false
-    idList = []
-
-    let cartLi = getAll('#container .cart_box ul li')
-    cartLi.forEach(ele => {
-        cartList.push(ele.dataset.id)
+function cartRemove(remBtn) {
+    remBtn.addEventListener('click', e => {
+        let id = e.currentTarget.parentElement.dataset.id;
+        removeFromCart(id);
+        e.currentTarget.parentElement.remove();
+        show();
     });
-    cartList = cartList.filter((value, index, self) => self.indexOf(value) === index);
-    show();
+}
 
-})
-
-// remove cart all (장바구니 비우기)
-$rem.addEventListener('click', e => {
-    let $cartLis = getAll('#container .cart_box ul li')
-    if ($cartLis.length) {
-        $cartLis.forEach(ele => {
-            ele.remove();
-        })
-        list.forEach(ele => {
-            ele.heart = false;
-            ele.cart = false;
-            ele.stock += ele.cartStock;
-            ele.cartStock = 0;
-        });
-        cartList = []
-        show()
-    }
-})
 
 // add cart (장바구니에 담기)
+// function add() {
+//     $add.forEach(ele => {
+//         ele.addEventListener('click', e => {
+//             let id = e.currentTarget.parentElement.dataset.id;
+//             if (cartList.includes(id)) {
+//                 return
+//             } else if (list[id].stock !== 0) {
+//                 list[id].cart = true;
+//                 list[id].stock--;
+//                 list[id].cartStock++;
+//                 let li = document.createElement('li');
+//                 let incBtn = document.createElement('button');
+//                 let redBtn = document.createElement('button');
+//                 let span = document.createElement('span');
+//                 let remBtn = document.createElement('button');
+//                 li.dataset.id = list[id].id
+//                 li.innerHTML = `
+//                 <img src="images/img${list[id].id}-0.jpg" alt="">
+//                 `
+//                 incBtn.classList.add('incre')
+//                 redBtn.classList.add('redu')
+//                 span.innerHTML = list[id].cartStock
+//                 remBtn.classList.add('remove')
+
+//                 incBtn.innerHTML = `<i class="xi-angle-up"></i>`
+//                 redBtn.innerHTML = `<i class="xi-angle-down"></i>`
+//                 remBtn.innerHTML = `<i class="xi-cart-remove"></i>`
+
+//                 $cartUl.append(li)
+//                 li.append(incBtn)
+//                 li.append(redBtn)
+//                 li.append(span)
+//                 li.append(remBtn)
+
+//                 cartPM(incBtn, redBtn)
+//                 cartRemove(remBtn)
+//                 cartList.push(id)
+//                 show();
+//             }
+//         })
+//     })
+// }
+function addToCart(id) {
+    if (!cartList.includes(id) && list[id].stock !== 0) {
+        list[id].cart = true;
+        updateStock(id, -1, 1);
+        cartList.push(id);
+    }
+}
+
 function add() {
     $add.forEach(ele => {
         ele.addEventListener('click', e => {
             let id = e.currentTarget.parentElement.dataset.id;
-            if (cartList.includes(id)) {
-                return
-            } else if (list[id].stock !== 0) {
-                list[id].cart = true;
-                list[id].stock--;
-                list[id].cartStock++;
+            if (!list[id].cart) {
+                addToCart(id);
                 let li = document.createElement('li');
                 let incBtn = document.createElement('button');
                 let redBtn = document.createElement('button');
                 let span = document.createElement('span');
                 let remBtn = document.createElement('button');
-                li.dataset.id = list[id].id
-                li.innerHTML = `
-                <img src="images/img${list[id].id}-0.jpg" alt="">
-                `
-                incBtn.classList.add('incre')
-                redBtn.classList.add('redu')
-                span.innerHTML = list[id].cartStock
-                remBtn.classList.add('remove')
 
-                incBtn.innerHTML = `<i class="xi-angle-up"></i>`
-                redBtn.innerHTML = `<i class="xi-angle-down"></i>`
-                remBtn.innerHTML = `<i class="xi-cart-remove"></i>`
+                li.dataset.id = list[id].id;
+                li.innerHTML = `<img src="images/img${list[id].id}-0.jpg" alt="">`;
+                incBtn.classList.add('incre');
+                redBtn.classList.add('redu');
+                span.innerHTML = list[id].cartStock;
+                remBtn.classList.add('remove');
 
-                $cartUl.append(li)
-                li.append(incBtn)
-                li.append(redBtn)
-                li.append(span)
-                li.append(remBtn)
+                incBtn.innerHTML = `<i class="xi-angle-up"></i>`;
+                redBtn.innerHTML = `<i class="xi-angle-down"></i>`;
+                remBtn.innerHTML = `<i class="xi-cart-remove"></i>`;
 
-                cartPM(incBtn, redBtn)
-                cartRemove(remBtn)
-                cartList.push(id)
+                $cartUl.append(li);
+                li.append(incBtn);
+                li.append(redBtn);
+                li.append(span);
+                li.append(remBtn);
+
+                cartPM(incBtn, redBtn);
+                cartRemove(remBtn);
                 show();
             }
-        })
-    })
+        });
+    });
 }
 
-// order cart (장바구니 주문하기)
-$goOrder.addEventListener('click', e => {
-    $bg.style.display = 'block'
-    $loading.style.display = 'block'
-
-    setTimeout(() => {
-        $bg.style.display = 'none'
-        $loading.style.display = 'none'
-        $wrap.style.display = 'none'
-        $wrapC.style.display = 'block'
-        showCart();
-        window.scrollTo(0, 0);
-    }, 1000);
-    $chk = getAll('#wrapB #container .cartUl_box ul li .xi-check-circle-o')
-})
-
-$ord.addEventListener('click', e => {
-    $bg.style.display = 'block'
-    $loading.style.display = 'block'
-
-    setTimeout(() => {
-        $bg.style.display = 'none'
-        $loading.style.display = 'none'
-        $wrap.style.display = 'none'
-        $wrapC.style.display = 'block'
-        showCart();
-        window.scrollTo(0, 0);
-    }, 1000);
-    $chk = getAll('#wrapB #container .cartUl_box ul li .xi-check-circle-o')
-})
-
-// more btn (더 보기 버튼)
-$more.addEventListener('click', e => {
-    uh += 635;
-    if (uh === 2520) {
-        uh = 2520;
-        $more.style.display = 'none'
-    }
-    $ul_box.style.height = uh + 'px';
-});
 
 // total price (총 금액)
 function totalPrice() {
@@ -475,6 +465,132 @@ function showCart() {
     })
     totalPrice()
 }
+
+// 찜 상품 담기
+$jjimCart.addEventListener('click', e => {
+    for (let i = 0; i < idList.length; i++) {
+        list[idList[i]].heart = true;
+    }
+    deduList = idList.filter(item => !cartList.includes(item));
+    if (deduList.length != 0) {
+        for (let i = 0; i < deduList.length; i++) {
+
+            list[deduList[i]].cart = true;
+            list[deduList[i]].stock--;
+            list[deduList[i]].cartStock++;
+            let li = document.createElement('li');
+            let incBtn = document.createElement('button');
+            let redBtn = document.createElement('button');
+            let span = document.createElement('span');
+            let remBtn = document.createElement('button');
+            li.dataset.id = list[deduList[i]].id
+            li.innerHTML = `
+            <img src="images/img${list[deduList[i]].id}-0.jpg" alt="">
+            `
+            incBtn.classList.add('incre')
+            redBtn.classList.add('redu')
+            span.innerHTML = list[deduList[i]].cartStock
+            remBtn.classList.add('remove')
+
+            incBtn.innerHTML = `<i class="xi-angle-up"></i>`
+            redBtn.innerHTML = `<i class="xi-angle-down"></i>`
+            remBtn.innerHTML = `<i class="xi-cart-remove"></i>`
+
+            $cartUl.append(li)
+            li.append(incBtn)
+            li.append(redBtn)
+            li.append(span)
+            li.append(remBtn)
+
+            cartPM(incBtn, redBtn)
+            cartRemove(remBtn)
+
+            list[deduList[i]].heart = false;
+        }
+        idList = []
+        cartList = []
+        deduList = []
+    } else {
+        idList = []
+        cartList = []
+        deduList = []
+    }
+    // heart remove
+    let heartElement = document.querySelector('.xi-heart');
+    if (heartElement) {
+        heartElement.classList.remove('xi-heart');
+        heartElement.classList.add('xi-heart-o');
+    }
+    // heart state false
+    idList = []
+
+    let cartLi = getAll('#container .cart_box ul li')
+    cartLi.forEach(ele => {
+        cartList.push(ele.dataset.id)
+    });
+    cartList = cartList.filter((value, index, self) => self.indexOf(value) === index);
+    show();
+
+})
+
+// remove cart all (장바구니 비우기)
+$rem.addEventListener('click', e => {
+    let $cartLis = getAll('#container .cart_box ul li')
+    if ($cartLis.length) {
+        $cartLis.forEach(ele => {
+            ele.remove();
+        })
+        list.forEach(ele => {
+            ele.heart = false;
+            ele.cart = false;
+            ele.stock += ele.cartStock;
+            ele.cartStock = 0;
+        });
+        cartList = []
+        show()
+    }
+})
+
+// order cart (장바구니 주문하기)
+$goOrder.addEventListener('click', e => {
+    $bg.style.display = 'block'
+    $loading.style.display = 'block'
+
+    setTimeout(() => {
+        $bg.style.display = 'none'
+        $loading.style.display = 'none'
+        $wrap.style.display = 'none'
+        $wrapC.style.display = 'block'
+        showCart();
+        window.scrollTo(0, 0);
+    }, 1000);
+    $chk = getAll('#wrapB #container .cartUl_box ul li .xi-check-circle-o')
+})
+
+$ord.addEventListener('click', e => {
+    $bg.style.display = 'block'
+    $loading.style.display = 'block'
+
+    setTimeout(() => {
+        $bg.style.display = 'none'
+        $loading.style.display = 'none'
+        $wrap.style.display = 'none'
+        $wrapC.style.display = 'block'
+        showCart();
+        window.scrollTo(0, 0);
+    }, 1000);
+    $chk = getAll('#wrapB #container .cartUl_box ul li .xi-check-circle-o')
+})
+
+// more btn (더 보기 버튼)
+$more.addEventListener('click', e => {
+    uh += 635;
+    if (uh === 2520) {
+        uh = 2520;
+        $more.style.display = 'none'
+    }
+    $ul_box.style.height = uh + 'px';
+});
 
 // logo click (로고 클릭)
 $logo.addEventListener('click', e => {
